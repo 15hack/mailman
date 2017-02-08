a198050d451c1f57a17fac42663e9102229a0972
15hack <15hack@riseup.net>
2017-02-08 23:33:39 +0100

    .

--- info_list/archive.py
+++ info_list/archive.py
@@ -4,33 +4,50 @@ import mailbox
 import re
 import fnmatch
 import os
+from datetime import datetime
+from email.utils import parsedate_tz
 
 mboxs = []
 for root, dirnames, filenames in os.walk('/var/lib/mailman/archives/private/'):
-    for filename in fnmatch.filter(filenames, '*.mbox'):
-        mboxs.append(os.path.join(root, filename))
+  for filename in fnmatch.filter(filenames, '*.mbox'):
+    mboxs.append(os.path.join(root, filename))
 
 campos=["From", "To"]
-correo=re.compile(r"[^@\s]+@[^@\s]+\.[^@\s]+")
+correo=re.compile(r"[^<\"@\s]+@[^@\s]+\.[^@\s>\",]+")
 
 data={}
 
+def to_datetime(datestring):
+    try:
+      time_tuple = parsedate_tz(datestring.strip())
+      dt = datetime(*time_tuple[:6])
+      return dt
+    except:
+      print datestring
+      return None
+
 for mbox in mboxs:
-	print mbox
-	mbox = mailbox.mbox(mbox)
-	for mail in mbox:
-		correos=[]
-		print mail['Date']
-		for campo in campos:
-			if mail[campo]:
-				correos = correos + correo.findall(mail[campo])
-		for i in range(len(correos)):
-			c=correos[i].strip()
-			if c[-1]==",":
-				c=c[0:-1]
-				correos[i]=c
-			while (c[0]=="<" and c[-1]==">") or (c[0]=="\"" and c[-1]=="\""):
-				c=c[1:-1]
-				correos[i]=c
-		correos=sorted(list(set(correos)))
-		print str(correos)
+  #print mbox
+  mbox = mailbox.mbox(mbox)
+  for mail in mbox:
+    if mail["From"] and "15hack@riseup.net" in mail["From"]:
+      continue
+    correos=[]
+    #print mail['Date']
+    date=to_datetime(mail['Date'])
+    if not date:
+      continue
+    #print str(date)
+    for campo in campos:
+      if mail[campo]:
+        correos = correos + [c.lower() for c in  correo.findall(mail[campo])]
+
+    correos=sorted(list(set(correos)))
+    for c in correos:
+      if not c in data or data[c]<date:
+        data[c]=date
+    #print str(correos)
+
+correos=sorted(data.keys())
+for c in correos:
+  print str(data[c])+"\t"+c
