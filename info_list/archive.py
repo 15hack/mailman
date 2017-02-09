799e26206689ffa7e91796d71f18fafc6a621258
15hack <15hack@riseup.net>
2017-02-09 23:00:29 +0100

    completar estadisticas con fecha de último uso

--- info_list/archive.py
+++ info_list/archive.py
@@ -7,6 +7,10 @@ import os
 from datetime import datetime
 from email.utils import parsedate_tz
 
+with open("list.txt") as f:
+    content = f.readlines()
+listas = [x.strip() for x in content] 
+
 mboxs = []
 for root, dirnames, filenames in os.walk('/var/lib/mailman/archives/private/'):
   for filename in fnmatch.filter(filenames, '*.mbox'):
@@ -20,6 +24,10 @@ data={}
 def to_datetime(datestring):
     try:
       time_tuple = parsedate_tz(datestring.strip())
+      if time_tuple[3]<0:
+        time_tuple[3]=-time_tuple[3]
+      if time_tuple[3]>23:
+         time_tuple[3]=23
       dt = datetime(*time_tuple[:6])
       return dt
     except:
@@ -33,16 +41,21 @@ for mbox in mboxs:
     if mail["From"] and "15hack@riseup.net" in mail["From"]:
       continue
     correos=[]
+
+    for campo in campos:
+      if mail[campo]:
+        correos = correos + [c.lower() for c in  correo.findall(mail[campo])]
+
+    correos=[c for c in list(set(correos)) if c in listas]
+    if len(correos)==0:
+       continue
+
     #print mail['Date']
     date=to_datetime(mail['Date'])
     if not date:
       continue
     #print str(date)
-    for campo in campos:
-      if mail[campo]:
-        correos = correos + [c.lower() for c in  correo.findall(mail[campo])]
 
-    correos=sorted(list(set(correos)))
     for c in correos:
       if not c in data or data[c]<date:
         data[c]=date
--- info_list/info.sh
+++ info_list/info.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+/usr/lib/mailman/bin/list_lists -b > list.txt
+python archive.py > last.txt
 for l in $(/usr/lib/mailman/bin/list_lists -b | sort); do
 	M=$(/usr/lib/mailman/bin/list_members "$l" | wc -l)
 	A=$(/usr/lib/mailman/bin/list_owners -m "$l" | wc -l)
-	echo "$M\t$A\t$l"
+	L=$(grep "$l" last.txt | cut -d' ' -f1)
+	echo "$M\t$A\t$L\t$l"
 done
