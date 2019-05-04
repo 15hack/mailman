c10ffccbf3732e4611811d5905048447b9d7a4ed
15hack <15hack@riseup.net>
2019-05-04 13:29:06 +0200

    pep8 y admitir indices

--- broad_mail/_add.py
+++ broad_mail/_add.py
@@ -1,2 +1,2 @@
 if not ('15hack@riseup.net' in mlist.accept_these_nonmembers):
-	mlist.accept_these_nonmembers.append('15hack@riseup.net')
+    mlist.accept_these_nonmembers.append('15hack@riseup.net')
--- broad_mail/build_msg.py
+++ broad_mail/build_msg.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python3
-import bs4
-import requests
+#!/usr/bin/env python3
+import re
 import sys
 from urllib.parse import urljoin
+
+import bs4
+import requests
 import urllib3
-import re
 
 urllib3.disable_warnings()
 requests.packages.urllib3.disable_warnings()
 
 re_blank = re.compile(r"\n\s*\n")
+
 
 def get(url):
     r = requests.get(url, verify=False)
@@ -20,22 +22,28 @@ def get(url):
             a.attrs["href"] = urljoin(url, href)
     return soup
 
-url = sys.argv[1] if len(sys.argv)==2 else None
-if url is None:
+
+url = sys.argv[1] if len(sys.argv) == 2 else None
+if url is None or url.isdigit():
     soup = get("https://15hack.tomalaplaza.net")
-    h1 = soup.find("h1", attrs={"class": "entry-title"})
+    index = (1 if url is None else int(url))-1
+    entradas = soup.findAll("h1", attrs={"class": "entry-title"})
+    if index < 0 or index >= len(entradas):
+        sys.exit("El indice ha de estar entre %s and %s" % (1, len(entradas)))
+    h1 = entradas[index]
     a = h1.find("a")
     url = a.attrs["href"]
 elif not url.startswith("http"):
-    sys.exit(url+ " no es una url valida")
+    sys.exit(url + " no es una url valida")
 
 
 def prt(s, *args, **kargv):
-    if len(args)>0:
+    if len(args) > 0:
         s = s.format(*args)
     s = s.lstrip()
     s = re_blank.sub("\n\n", s)
     print(s, **kargv)
+
 
 soup = get(url)
 h1 = soup.find("h1", attrs={"class": "entry-title"})
@@ -51,7 +59,7 @@ content = soup.find("div", attrs={"class": "entry-content"})
 for a in content.findAll("a"):
     txt = a.get_text().strip()
     url = a.attrs.get("href", None)
-    if len(txt)==0 or url is None or url.startswith("#"):
+    if len(txt) == 0 or url is None or url.startswith("#"):
         a.unwrap()
     else:
         if txt in url:
