6e884cc4723e14b0a2ba08d4eee38bca01068581
15hack <15hack@riseup.net>
2019-05-05 15:51:32 +0200

    mejorar el formato

--- broad_mail/build_msg.py
+++ broad_mail/build_msg.py
@@ -11,7 +11,7 @@ urllib3.disable_warnings()
 requests.packages.urllib3.disable_warnings()
 
 re_blank = re.compile(r"\n\s*\n")
-
+heads = ["h" +str(h) for h in range(1,7)]
 
 def get(url):
     r = requests.get(url, verify=False)
@@ -37,9 +37,14 @@ elif not url.startswith("http"):
     sys.exit(url + " no es una url valida")
 
 
-def prt(s, *args, **kargv):
-    if len(args) > 0:
-        s = s.format(*args)
+def prt(*args, **kargv):
+    args = list(args)
+    for i, a in enumerate(args):
+        if isinstance(a, bs4.Tag):
+            args[i] = a.get_text().strip()
+    s = args[0]
+    if len(args) > 1:
+        s = s.format(*args[1:])
     s = s.lstrip()
     s = re_blank.sub("\n\n", s)
     print(s, **kargv)
@@ -53,7 +58,7 @@ From: 15hack@riseup.net
 Subject: {0}
 
 Share link: {1}
-''', h1.get_text().strip(), url)
+''', h1, url)
 
 content = soup.find("div", attrs={"class": "entry-content"})
 for a in content.findAll("a"):
@@ -66,8 +71,15 @@ for a in content.findAll("a"):
             a.string = url
         else:
             a.append(" (%s)" % url)
-for n in content.findAll(["p"]):
+i = 1
+for hs in heads:
+    hs = content.findAll(hs)
+    if len(hs)>0:
+        for h in hs:
+            h.insert(0 ,("#" * i)+" ")
+        i + 1
+for n in content.findAll(["p"] + heads):
     n.append("\n")
 for n in content.findAll(["li"]):
     n.insert(0, "- ")
-prt(content.get_text().strip())
+prt(content)
