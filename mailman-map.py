e21e9cdfd93a725321c7b3ae2a27bc282eabf2a0
15hack <15hack@riseup.net>
2020-11-24 13:24:30 +0100

    capturar mas mails

--- mailman-map.py
+++ mailman-map.py
@@ -110,8 +110,8 @@ def get_mails(l):
         for m in mbox:
             if m["From"] and "15hack@riseup.net" in m["From"]:
                 continue
-            if n not in (m["From"] or "") and n not in (m["To"] or ""):
-                continue
+            #if n not in (m["From"] or "").lower() and n not in (m["To"] or "").lower():
+            #    continue
             yield m
 
 
